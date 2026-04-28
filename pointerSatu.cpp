7164c8c7d6deb1a0464f6e0c8ffdb15714fee916
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-04-28 19:18:49 +0700

    membuat fungsi utama int main dan menambah object mhs

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa
-{
-public:
+class mahasiswa{
+    public:
     int nim;
-    void showNim()
-    {
+    void showNim(){
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main (){
+    mahasiswa mhs{1};// Object mhs
+}
