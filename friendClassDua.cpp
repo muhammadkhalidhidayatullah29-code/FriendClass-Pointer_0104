8ebe67083170556d6c77a1f1516ab0d30a8b040d
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-04-28 19:06:18 +0700

    menambah void manusia

--- friendClassDua.cpp
+++ friendClassDua.cpp
@@ -5,7 +5,7 @@ class pelajar;
 class manusia
 {
 public:
-    void showNilaiPelajar(pelajar &p);
+    void showNilaiPelajar(pelajar &x);
 };
 
 class pelajar
@@ -15,5 +15,10 @@ private:
 
 public:
     pelajar() { nilai = 100; }
-    friend void manusia::showNilaiPelajar(pelajar &p);
+    friend void manusia::showNilaiPelajar(pelajar &x);
 };
+
+void manusia ::showNilaiPelajar(pelajar &x)
+{
+    cout << x.nilai;
+}
